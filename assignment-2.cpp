4b842613c034e3e1be18604c781b3560521aaee8
Sihle <msihlesndlovu97@gmail.com>
2025-05-26 20:49:20 +0200

    hardcoded the seating arrangements for displayAvailableSeats() and replaced \n\n with << endl << endl; instead

--- assignment-2.cpp
+++ assignment-2.cpp
@@ -16,6 +16,19 @@ void displayFlightMenu()
     cout << left << setw(8) << "4." << setw(12) << "13:00" << setw(12) << "15:30" << endl;
     cout << left << setw(8) << "5." << setw(12) << "15:00" << setw(12) << "17:30" << endl;
 }
+void displayAvailableSeats()
+{
+    cout << "First Class (R1920) " << endl;
+    cout << "|A1|A2|A3|-----|A4|A5|A6|" << endl;
+    cout << "|B1|B2|B3|-----|B4|B5|B6|" << endl;
+    cout << "|C1|C2|C3|-----|C4|C5|C6|" << endl;
+    cout << "Economy Class (R1600)" << endl;
+    cout << "|D1|D2|D3|-----|D4|D5|D6|" << endl;
+    cout << "|E1|E2|E3|-----|E4|E5|E6|" << endl;
+    cout << "|F1|F2|F3|-----|F4|F5|F6|" << endl;
+    cout << "|I1|I2|" << endl
+         << endl;
+}
 int main()
 {
     // 1. prompt user to input their full name
@@ -24,7 +37,8 @@ int main()
     cout << "\nPlease enter your full name: ";
     getline(cin, fullName);
     cout << "\n"
-         << fullName << ", welcome to the COS1511 Flight Booking System\n\n";
+         << fullName << ", welcome to the COS1511 Flight Booking System" << endl
+         << endl;
     // 2. display a menu showing the available times for the flight. Then validate the selected time
     do
     {
@@ -39,7 +53,14 @@ int main()
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
-        cout << "\nYou have selected option " << preferredDepartureTime << endl;
-    } while (false);
+        cout << "You have selected option " << preferredDepartureTime << endl
+             << endl;
+    } while (false); // (false) ensures that the displayFlightMenu() and cout << will only be executed once
+
+    // seat selection
+    cout << "The available seats for flight option " << preferredDepartureTime << " are as follows: " << endl
+         << endl;
+    displayAvailableSeats();
+    cout << "Please key in a seat number to choose a seat(A2): " << endl;
     return 0;
 }
