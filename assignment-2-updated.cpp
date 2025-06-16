6aae7b29e1d7ea4a529663c69fb0e5c9b65fd603
Sihle <msihlesndlovu97@gmail.com>
2025-06-16 18:39:17 +0200

    completed question 4

--- assignment-2-updated.cpp
+++ assignment-2-updated.cpp
@@ -2,12 +2,39 @@
 #include <string>
 using namespace std;
 
+// prototype 1
+void inputOfData(string &name, string &addressLine1, string &addressLine2, string &postalCode);
+
+// prototype 2
+void returnData(string &name, string &addressLine1, string &addressLine2, string &postalCode);
+
 int main()
 {
+    string name, addressLine1, addressLine2, PostalCode;
+    cout << "---- Enter Postal Address Details ----\n\n";
+    inputOfData(name, addressLine1, addressLine2, PostalCode);
+    returnData(name, addressLine1, addressLine2, PostalCode);
+    return 0;
 }
 
 void inputOfData(string &name, string &addressLine1, string &addressLine2, string &postalCode)
 {
-    cout << "Enter your name (e.g. Mr S.M. Ndlovu): ";
+    cout << "Enter your Name (e.g. Mr S.M. Ndlovu): ";
     getline(cin, name);
+    cout << "Enter Address Line 1 (e.g. P.O. Box 50741): ";
+    getline(cin, addressLine1);
+    cout << "Enter your Address Line 2 (e.g. Sandton): ";
+    getline(cin, addressLine2);
+    cout << "Enter your Postal Code (e.g. 3201): ";
+    getline(cin, postalCode);
+}
+
+void returnData(string &name, string &addressLine1, string &addressLine2, string &postalCode)
+{
+    cout << "\n\n---- Here are your address details ----\n";
+    cout << name << endl;
+    cout << addressLine1 << endl;
+    cout << addressLine2 << endl;
+    cout << postalCode << endl;
+    cout << "---- ---- ----" << endl;
 }
