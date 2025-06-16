4f0015ecee115d9cca77a17b9a0fb9195e5cc8a8
Sihle <msihlesndlovu97@gmail.com>
2025-06-16 18:49:40 +0200

    updated aesthetics

--- assignment-2-updated.cpp
+++ assignment-2-updated.cpp
@@ -3,38 +3,37 @@
 using namespace std;
 
 // prototype 1
-void inputOfData(string &name, string &addressLine1, string &addressLine2, string &postalCode);
+void inputData(string &name, string &addr1, string &addr2, string &postalCode);
 
 // prototype 2
-void returnData(string &name, string &addressLine1, string &addressLine2, string &postalCode);
+void displayData(string &name, string &addr1, string &addr2, string &postalCode);
 
 int main()
 {
-    string name, addressLine1, addressLine2, PostalCode;
+    string name, addr1, addr2, PostalCode;
     cout << "---- Enter Postal Address Details ----\n\n";
-    inputOfData(name, addressLine1, addressLine2, PostalCode);
-    returnData(name, addressLine1, addressLine2, PostalCode);
+    inputData(name, addr1, addr2, PostalCode);
+    displayData(name, addr1, addr2, PostalCode);
     return 0;
 }
 
-void inputOfData(string &name, string &addressLine1, string &addressLine2, string &postalCode)
+void inputData(string &name, string &addr1, string &addr2, string &postalCode)
 {
-    cout << "Enter your Name (e.g. Mr S.M. Ndlovu): ";
+    cout << "Enter your Name: ";
     getline(cin, name);
-    cout << "Enter Address Line 1 (e.g. P.O. Box 50741): ";
-    getline(cin, addressLine1);
-    cout << "Enter your Address Line 2 (e.g. Sandton): ";
-    getline(cin, addressLine2);
-    cout << "Enter your Postal Code (e.g. 3201): ";
+    cout << "Enter Address Line 1: ";
+    getline(cin, addr1);
+    cout << "Enter your Address Line 2: ";
+    getline(cin, addr2);
+    cout << "Enter your Postal Code: ";
     getline(cin, postalCode);
 }
 
-void returnData(string &name, string &addressLine1, string &addressLine2, string &postalCode)
+void displayData(string &name, string &addr1, string &addr2, string &postalCode)
 {
     cout << "\n\n---- Here are your address details ----\n";
     cout << name << endl;
-    cout << addressLine1 << endl;
-    cout << addressLine2 << endl;
+    cout << addr1 << endl;
+    cout << addr2 << endl;
     cout << postalCode << endl;
-    cout << "---- ---- ----" << endl;
 }
